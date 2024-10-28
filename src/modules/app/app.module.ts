6b998ca228b7e60becddca609491e487b8881e4c
pendulum <rs.pendulum@gmail.com>
2024-10-28 18:09:32 +0300

    tests are ready transactions with mock

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ExampleModule } from '../example/example.module';
 import { SchemaModule } from 'src/schema/schema.module';
 import { AuthModule } from '../auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SchemaModule,
     AuthModule,
     ExampleModule
--- test/app.e2e.ts
+++ test/app.e2e.ts
@@ -1,16 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication, ModuleMetadata, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { DataSource, QueryRunner, Repository } from 'typeorm';
 import { dataSourceUserOption } from '../src/schema/datasource';
 import { User } from '../src/schema/users/user.entity';
 import { JwtToken } from '../src/schema/jwt-tokens/jwt.token.entity';
-import { getRepositoryToken } from '@nestjs/typeorm';
+import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import { AppModule } from '../src/modules/app/app.module';
-import { SchemaModule } from '../src/schema/schema.module';
+import { CreateDtoReq } from 'src/modules/auth/auth-email/registration/dto';
+import { faker } from '@faker-js/faker';
+import { randomUUID } from 'crypto';
+import { EmailService } from 'src/modules/auth/auth-email/services/email/email.service';
+import { AuthenticationModule } from 'src/modules/auth/auth-email/authentication/authentication.module';
+import { AuthenticationService } from 'src/modules/auth/auth-email/authentication/authentication.service';
+import { UsersDBModule } from 'src/schema/users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
+
+function getRandomIndex(size: number) {
+  return Math.floor(Math.random() * size)
+}
 
 describe('Tests (e2e)', () => {
+  const mockEmailService = {
+    sendActivationMail: jest.fn(),
+    sendRecoveryMail: jest.fn(),
+  };
+
   let app: INestApplication;
   let dataSource: DataSource;
   let transactionRunner: QueryRunner;
@@ -22,30 +38,25 @@ describe('Tests (e2e)', () => {
     dataSource = new DataSource(dataSourceUserOption);
     await dataSource.initialize();
     transactionRunner = dataSource.manager.connection.createQueryRunner();
-    await transactionRunner.startTransaction();
+    await transactionRunner.startTransaction('SERIALIZABLE');
 
     usersRepository = transactionRunner.manager.getRepository(User);
     jwtRepository = transactionRunner.manager.getRepository(JwtToken);
 
-  });
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    const metaData: ModuleMetadata = {
       imports: [
         AppModule,
-        SchemaModule
       ],
-      providers: [
-        {
-          provide: getRepositoryToken(User),
-          useValue: usersRepository,
-        },
-        {
-          provide: getRepositoryToken(JwtToken),
-          useValue: jwtRepository,
-        },
-      ],
-    }).compile();
+    }
+
+    const moduleFixture: TestingModule = await Test
+      .createTestingModule(metaData)
+      .overrideProvider(getRepositoryToken(JwtToken))
+      .useValue(jwtRepository)
+      .overrideProvider(getRepositoryToken(User))
+      .useValue(usersRepository)
+      .compile();
 
     app = moduleFixture.createNestApplication();
 
@@ -55,21 +66,54 @@ describe('Tests (e2e)', () => {
         forbidNonWhitelisted: true,
       }),
     );
-
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    const t = request(app.getHttpServer(), {})
+  beforeEach(async () => {
+
+  });
+  it('[create user] /auth/create-account POST ', async () => {
+    const size = 2;
+    const testArr = Array.from({ length: size }, () => {
+
+      const randomIndex = getRandomIndex(8);
+      const passwordArr = Array.from(faker.internet.password({ length: 16 }))
+      passwordArr[randomIndex] = passwordArr[randomIndex].toUpperCase();
+
+      const email = faker.internet
+        .email({ firstName: randomUUID() })
+        .replaceAll('-', '');
+
+      const password = passwordArr.join('');
+      return {
+        email: email,
+        password: password
+      } as CreateDtoReq
+    })
+    for (const input of testArr) {
+      const ter = request(app.getHttpServer(), {})
+        .post('/auth/create-account')
+
+      for (const [key, value] of Object.entries(input)) {
+        ter.field(key, value);
+      }
+      await ter.expect(201);
+    }
+  });
+
+  it('/ (GET)', async () => {
+    return request(app.getHttpServer(), {})
       .post('/auth/login')
       .field('email', 'example@email.com')
       .field('password', 'Qwerty!1')
       .expect(201)
-      .expect('Hello World!');
   });
+  afterEach(async () => {
+    await transactionRunner.rollbackTransaction();
+  })
 
   afterAll(async () => {
-    await transactionRunner.rollbackTransaction();
+    await transactionRunner.release();
     await dataSource.destroy();
   });
 });
